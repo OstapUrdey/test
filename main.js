49236dfb1830fba6fb6635cf89b06a386b146558
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-07-04 23:21:31 +0300

    test

---
+++ main.js
@@ -0,0 +1,37 @@
+// function sum (a, b) {
+//     return a + b;
+// }
+// let result = sum(5, "5");
+// console.log(result);
+
+// function reapeatString (str, num) {
+//     return str.repeat(num);
+// }
+// let result = reapeatString("Hi", 5);
+// console.log(result);
+
+// function average (arr) {
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//      sum += arr[i];
+// }
+// return sum / arr.length;
+// }
+// let result = average([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(result);
+
+// function reverseDirection (string) {
+// return string.split("").reverse().join("");
+// }
+// let result = reverseDirection("hello");
+// console.log(result);
+
+// function squareArray (arr) {
+//     let squareArray = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         squareArray.push(arr[i]*arr[i]);
+//     }
+//     return squareArray;
+// }
+// let result = squareArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(result);
